19dc9d0daa779b3d00646642e373e0024e0090ad
seonghye <tjdgp0817@gmail.com>
2023-09-04 15:29:23 +0900

    이메일 테이블 값 보내기

--- app.py
+++ app.py
@@ -1,3 +1,192 @@
+# import numpy as np
+# import json
+# from flask import Flask, request, jsonify, render_template
+# import pandas as pd
+# import numpy as np
+# import pvlib
+# from datetime import datetime, timedelta
+# import smtplib, ssl, imaplib
+# from email.mime.multipart import MIMEMultipart
+# from email.mime.text import MIMEText
+
+# id = pd.read_excel('./id_password.xlsx', header=None)
+
+# email_user = id[0][0]
+# email_password = id[1][0]
+
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
+
+# @app.route('/test')
+# def test():
+#     return jsonify({
+#         'result': 'abvdcdsa'
+#     })
+
+# @app.route('/abc')
+# def abc():
+#     return 'abc'
+
+# @app.route('/about')
+# def about():
+#    return render_template('about.html')
+
+# @app.route('/contact')
+# def contact():
+#    return render_template('contact.html')
+
+# @app.route('/index')
+# def index():
+#    return render_template('index.html')
+
+# @app.route('/map')
+# def map():
+#    return render_template('map.html')
+
+# @app.route('/nav')
+# def nav():
+#    return render_template('nav.html')
+
+# @app.route('/tech')
+# def tech():
+#    return render_template('tech.html')
+
+# @app.route('/some')
+# def home():
+#    return render_template('some.html')
+
+# @app.route('/clear_sky', methods=['POST'])
+# def clear_sky():
+#     data = request.get_json()
+#     capacity = data['capacity']
+#     latitude = data['latitude']
+#     longitude = data['longitude']
+
+#     today = datetime.today()
+
+#     start_date = today.replace(hour=0)
+#     end_date = today.replace(hour=23)
+
+#     location = pvlib.location.Location(latitude, longitude, tz='Asia/Seoul')
+
+#     times = pd.date_range(start=start_date, end=end_date, freq='1H', tz='Asia/Seoul')
+
+#     solpos = location.get_solarposition(times=times)
+#     dni_extra = pvlib.irradiance.get_extra_radiation(times)
+#     airmass = pvlib.atmosphere.get_relative_airmass(solpos['apparent_zenith'])
+#     pressure = pvlib.atmosphere.alt2pres(location.altitude)
+#     am_abs = pvlib.atmosphere.get_absolute_airmass(airmass, pressure)
+#     tl = pvlib.clearsky.lookup_linke_turbidity(times, latitude, longitude)
+
+#     solis_clearsky = pvlib.clearsky.simplified_solis(solpos['apparent_zenith'], am_abs, tl)
+#     cs = location.get_clearsky(times, model='simplified_solis')
+
+#     capacity = float(capacity)
+
+#     system = pvlib.pvsystem.PVSystem(surface_tilt=25, surface_azimuth=180,
+#                                      module_parameters={'pdc0': capacity, 'gamma_pdc': -0.04},
+#                                      inverter_parameters={'pdc0': capacity},
+#                                      modules_per_string=1, strings_per_inverter=1,
+#                                      temperature_model_parameters={'a': -3.56, 'b': -0.075, 'deltaT': 3})
+#     mc = pvlib.modelchain.ModelChain(system, location, spectral_model='no_loss', aoi_model='physical')
+
+#     mc.run_model(solis_clearsky)
+
+#     # Replace NaN values in the 'ac' column with 0
+
+#     # mc.ac.fillna(0, inplace=True)
+
+#     df = pd.DataFrame(mc.results.ac).to_dict('records')
+
+#     return jsonify({
+#         'result': [record for record in df]
+#     })
+
+# @app.route('/process', methods=['POST'])
+# def process():
+#     data = request.get_json()
+#     input_variable = data['input_variable']
+#     # 받은 input_variable 값을 사용하여 원하는 작업 수행 (여기서는 간단히 문자열을 변환하여 리턴)
+#     result = process_data(input_variable)
+#     return jsonify({'result': result})
+
+# def process_data(input_variable):
+#     # 원하는 작업을 수행하고 결과를 리턴하는 함수
+#     return f'입력된 변수 값: {input_variable}'
+
+
+# def connect_email(user_id):
+#     """
+#     input:
+#         user_id : gmail id (dtype : str)
+
+#     사용시기:
+#         메일을 보낼때 연결을 위해서 사용
+#         보내는 메일은 smtp를 사용
+#     """
+#     smtp = smtplib.SMTP('smtp.gmail.com', 587) # send
+
+#     smtp.ehlo()
+#     smtp.starttls()
+#     smtp.login(user_id, email_password)
+
+#     return smtp
+
+# def send_email(smtp, recipient, subject, body):
+#     """
+#     smtp : smtp connection (dtype : smtplib.SMTP)
+#     recipient : 수신자 (dtype : str or list)
+#     subject : 제목 (dtype : str)
+#     body : 본문 (dtype : str)
+#     """
+#     FROM = email_user
+#     TO = recipient if isinstance(recipient, list) else [recipient]
+#     SUBJECT = subject
+#     TEXT = body
+
+#     # Prepare actual message
+#     message = MIMEMultipart("alternative", None, [MIMEText(TEXT, 'html', 'utf-8')])
+
+#     message['Subject'] = SUBJECT
+#     message['From'] = FROM
+#     message['To'] = ", ".join(TO)
+
+#     # Send the mail using the passed smtp connection
+#     smtp.sendmail(FROM, TO, message.as_string())
+
+# @app.route('/contact_mail', methods=['POST'])
+# def send_mail():
+#     smtp = connect_email(email_user)
+
+#     data = request.get_json()
+
+#     # 데이터에서 이름, 연락처, 이메일, 문의 내용을 추출합니다.
+#     name = data.get('name')
+#     contact = data.get('contact')
+#     email = data.get('email')
+#     query = data.get('query')
+
+#     # 이메일 내용 구성
+#     subject = "새로운 문의가 도착했습니다!"
+#     body = f"이름: {name}<br>연락처: {contact}<br>이메일: {email}<br>문의 내용: {query}"
+
+#     recipient_email = "jang0212@tukorea.ac.kr"  # Please replace "EMAIL" with your actual email
+
+#     # 이메일 전송
+#     # try:
+#     send_email(smtp, recipient_email, subject, body)
+#         # return jsonify({'status': 'success', 'message': 'Email sent successfully!'})
+#     # except Exception as e:
+#     #     return jsonify({'status': 'error', 'message': str(e)})
+
+
+# if __name__ == '__main__':
+#     app.run(debug = True, port=5000)
+
 import numpy as np
 import json
 from flask import Flask, request, jsonify, render_template
@@ -5,6 +194,15 @@ import pandas as pd
 import numpy as np
 import pvlib
 from datetime import datetime, timedelta
+import smtplib, ssl, imaplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+id = pd.read_excel("./id_password.xlsx", header=None)
+
+email_user = id[0][0]
+email_password = id[1][0]
+
 
 app = Flask(__name__)
 
@@ -90,13 +288,13 @@ def clear_sky():
     capacity = float(capacity)
 
     system = pvlib.pvsystem.PVSystem(
-        surface_tilt=15,
+        surface_tilt=25,
         surface_azimuth=180,
-        module_parameters={"pdc0": capacity, "gamma_pdc": -0.004},
+        module_parameters={"pdc0": capacity, "gamma_pdc": -0.04},
         inverter_parameters={"pdc0": capacity},
         modules_per_string=1,
         strings_per_inverter=1,
-        temperature_model_parameters={"a": -3.56, "b": -0.075, "deltaT": 5},
+        temperature_model_parameters={"a": -3.56, "b": -0.075, "deltaT": 3},
     )
     mc = pvlib.modelchain.ModelChain(
         system, location, spectral_model="no_loss", aoi_model="physical"
@@ -127,9 +325,94 @@ def process_data(input_variable):
     return f"입력된 변수 값: {input_variable}"
 
 
-# @app.route('/contact_mail')
-# def connect_mail():
-#     data = request.get_json()
+def connect_email(user_id):
+    """
+    input:
+        user_id : gmail id (dtype : str)
+
+    사용시기:
+        메일을 보낼때 연결을 위해서 사용
+        보내는 메일은 smtp를 사용
+    """
+    smtp = smtplib.SMTP("smtp.gmail.com", 587)  # send
+
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login(user_id, email_password)
+
+    return smtp
+
+
+def send_email(smtp, recipient, subject, body):
+    """
+    smtp : smtp connection (dtype : smtplib.SMTP)
+    recipient : 수신자 (dtype : str or list)
+    subject : 제목 (dtype : str)
+    body : 본문 (dtype : str)
+    """
+    FROM = email_user
+    TO = recipient if isinstance(recipient, list) else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = MIMEMultipart("alternative", None, [MIMEText(TEXT, "html", "utf-8")])
+
+    message["Subject"] = SUBJECT
+    message["From"] = FROM
+    message["To"] = ", ".join(TO)
+
+    # Send the mail using the passed smtp connection
+    smtp.sendmail(FROM, TO, message.as_string())
+
+
+@app.route("/contact_mail", methods=["POST"])
+def send_mail():
+    smtp = connect_email(email_user)
+
+    data = request.get_json()
+
+    # 데이터에서 이름, 연락처, 이메일, 문의 내용을 추출합니다.
+    name = data.get("name")
+    contact = data.get("contact")
+    email = data.get("email")
+    query = data.get("query")
+
+    # 이메일 내용 구성
+    subject = "새로운 문의가 도착했습니다!"
+    body = f"이름: {name}<br>연락처: {contact}<br>이메일: {email}<br>문의 내용: {query}"
+
+    recipient_email = "jang0212@tukorea.ac.kr"
+
+    # 이메일 전송
+    try:
+        send_email(smtp, recipient_email, subject, body)
+        return jsonify({"status": "success", "message": "Email sent successfully!"})
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)})
+
+    # smtp = connect_email(email_user)
+
+    # data = request.get_json()
+
+    # # 데이터에서 이름, 연락처, 이메일, 문의 내용을 추출합니다.
+    # name = data.get('name')
+    # contact = data.get('contact')
+    # email = data.get('email')
+    # query = data.get('query')
+
+    # # 이메일 내용 구성
+    # subject = "새로운 문의가 도착했습니다!"
+    # body = f"이름: {name}<br>연락처: {contact}<br>이메일: {email}<br>문의 내용: {query}"
+
+    # recipient_email = "jang0212@tukorea.ac.kr"  # Please replace "EMAIL" with your actual email
+
+    # # 이메일 전송
+    # # try:
+    # send_email(smtp, recipient_email, subject, body)
+    #     # return jsonify({'status': 'success', 'message': 'Email sent successfully!'})
+    # # except Exception as e:
+    # #     return jsonify({'status': 'error', 'message': str(e)})
 
 
 if __name__ == "__main__":
--- static/js/main.js
+++ static/js/main.js
@@ -1,45 +1,188 @@
+// // static/js/main.js
+// function sendData() {
+//     const inputVariable = document.getElementById('input_variable').value;
+//     fetch('/clear_sky', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//         'capacity': capacity,
+//         'latitude': latitude,
+//         'longitude': longitude,
+//     }),
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         // 서버로부터 받은 응답 처리
+//         const resultElement = document.getElementById('result');
+//         resultElement.innerHTML = ''; // 결과 영역 초기화
+
+//         // Create table
+//         let table = document.createElement('table');
+//         let headerRow = document.createElement('tr');
+//         let dataRow = document.createElement('tr');
+
+//         // Add headers and data
+//         data.result.forEach((item, index) => {
+//             let th = document.createElement('th');
+//             th.textContent = `${index}시`;  // assuming each item represents an hour
+//             headerRow.appendChild(th);
+
+//             let td = document.createElement('td');
+//             td.textContent = Object.values(item)[0];  // assuming each item is a single-value object
+//             dataRow.appendChild(td);
+//         });
+
+//         table.appendChild(headerRow);
+//         table.appendChild(dataRow);
+
+//         resultElement.appendChild(table);
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//     });
+// }
+
+// function submitData() {
+//     let userData = {};
+
+//     // 이름, 연락처, 이메일, 문의내용을 가져옵니다.
+//     userData.name = document.getElementById('user').value;
+//     userData.contact = document.getElementById('user-number').value;
+
+//     // 이메일 주소는 두 부분으로 나뉘어져 있으므로 합쳐야 합니다.
+//     let emailPart1 = document.getElementById('user-email').value;
+//     let emailPart2 = document.getElementById('user-email2').value;
+//     userData.email = `${emailPart1}@${emailPart2}`;
+
+//     userData.query = document.getElementById('ask').value;
+
+//     // 서버로 데이터 전송
+//     fetch('/contact_mail', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(userData)
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch((error) => {
+//         console.error('Error:', error);
+//     });
+// }
+
 // static/js/main.js
 function sendData() {
-    const inputVariable = document.getElementById('input_variable').value;
-    fetch('/clear_sky', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-        'capacity': capacity,
-        'latitude': latitude,
-        'longitude': longitude, 
+  const inputVariable = document.getElementById("input_variable").value;
+  fetch("/clear_sky", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      capacity: capacity,
+      latitude: latitude,
+      longitude: longitude,
     }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // 서버로부터 받은 응답 처리
+      const resultElement = document.getElementById("result");
+      resultElement.innerHTML = ""; // 결과 영역 초기화
+
+      // Create table
+      let table = document.createElement("table");
+      let headerRow = document.createElement("tr");
+      let dataRow = document.createElement("tr");
+
+      // Add headers and data
+      data.result.forEach((item, index) => {
+        let th = document.createElement("th");
+        th.textContent = `${index}시`; // assuming each item represents an hour
+        headerRow.appendChild(th);
+
+        let td = document.createElement("td");
+        td.textContent = Object.values(item)[0]; // assuming each item is a single-value object
+        dataRow.appendChild(td);
+      });
+
+      table.appendChild(headerRow);
+      table.appendChild(dataRow);
+
+      resultElement.appendChild(table);
     })
-    .then(response => response.json())
-    .then(data => {
-        // 서버로부터 받은 응답 처리
-        const resultElement = document.getElementById('result');
-        resultElement.innerHTML = ''; // 결과 영역 초기화
-
-        // Create table
-        let table = document.createElement('table');
-        let headerRow = document.createElement('tr');
-        let dataRow = document.createElement('tr');
-
-        // Add headers and data
-        data.result.forEach((item, index) => {
-            let th = document.createElement('th');
-            th.textContent = `${index}시`;  // assuming each item represents an hour
-            headerRow.appendChild(th);
-
-            let td = document.createElement('td');
-            td.textContent = Object.values(item)[0];  // assuming each item is a single-value object
-            dataRow.appendChild(td);
-        });
-
-        table.appendChild(headerRow);
-        table.appendChild(dataRow);
-
-        resultElement.appendChild(table);
-    })
-    .catch(error => {
-        console.error('Error:', error);
+    .catch((error) => {
+      console.error("Error:", error);
     });
 }
+
+function submitData() {
+  // Collect user input
+  let name = document.getElementById("user").value;
+  let contact = document.getElementById("user-number").value;
+  let email =
+    document.getElementById("user-email").value +
+    "@" +
+    document.getElementById("user-email2").value;
+  let query = document.getElementById("ask").value;
+
+  // Construct the data object
+  let userData = {
+    name: name,
+    contact: contact,
+    email: email,
+    query: query,
+  };
+
+  // Send POST request to the server
+  fetch("/contact_mail", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status === "success") {
+        alert(data.message);
+      } else {
+        alert("Error: " + data.message);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("An error occurred. Please try again.");
+    });
+}
+// 이름, 연락처, 이메일, 문의내용을 가져옵니다.
+userData.name = document.getElementById("user").value;
+userData.contact = document.getElementById("user-number").value;
+
+// 이메일 주소는 두 부분으로 나뉘어져 있으므로 합쳐야 합니다.
+let emailPart1 = document.getElementById("user-email").value;
+let emailPart2 = document.getElementById("user-email2").value;
+userData.email = `${emailPart1}@${emailPart2}`;
+
+userData.query = document.getElementById("ask").value;
+
+// 서버로 데이터 전송
+fetch("/contact_mail", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(userData),
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
